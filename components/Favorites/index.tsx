5a8c5fc57f57c8103b507173dd078e4313fea08e
jswill88 <joshua.williams@simpleviewinc.com>
2022-12-08 12:00:40 -0800

    Fix bug and update favorites

--- components/Favorites/index.tsx
+++ components/Favorites/index.tsx
@@ -3,22 +3,18 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useAppContext } from "state/AppContext";
 import { ComicProps } from "types";
 import styles from "styles/Favorites.module.css";
-import { animated, useTransition, config } from 'react-spring';
+import { animated, useTransition } from 'react-spring';
 
 export function Favorites() {
 	const { favorites, toggleFavorite } = useAppContext();
 	const favoritesArr: ComicProps[] = favorites ? Object.values(favorites) : [];
 
 	const transitions = useTransition(favoritesArr, {
-		from: { opacity: 0, life: '0%' },
-		enter: () => async (next) => {
-			await next({ life: '100%' })
-			await next({ opacity: 1 })
-		},
-		leave: { opacity: 0 },
+		from: { opacity: 0, life: '0%', marginBottom: .2 },
+		enter: { opacity: 1, marginBottom: 0 },
+		leave: { opacity: 0, marginBottom: 1 },
 		keys: ({ id }) => id,
 		sort: (a, b) => a.title.toLowerCase() < b.title.toLowerCase() ? -1 : 1,
-		config: config.stiff
 	})
 
 	const formattedThumbnailPath = ({ path, extension }) => {
@@ -32,8 +28,14 @@ export function Favorites() {
 		<div className={styles.container}>
 			<h3 className={styles.title}>Favorites</h3>
 			<div className={styles.cards}>
-				{transitions(({opacity}, item?:ComicProps) => (
-					<animated.div className={styles.card} style={{ opacity: opacity.to({ range: [0,1], output: [ 0,1 ]}) }} >
+				{transitions(({opacity, marginBottom}, item?: ComicProps) => (
+					<animated.div
+						className={styles.card}
+						style={{
+							opacity: opacity.to({ range: [0,1], output: [0,1]}),
+							marginBottom: marginBottom.to({ range: [0,1], output: ['0','-300px']})
+						}}
+					>
 						<button
 							onClick={() => toggleFavorite({ id: item.id })}
 							className={styles.button}
